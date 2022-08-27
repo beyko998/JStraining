db2a1892bdff374623cb9c290e0ea2759ecc34b2
Andoryu <beyko998@gmail.com>
2022-08-27 17:45:02 +0300

    1st commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+"use strict";
+// let number = 5;
+// const leftBorderWidth =1;
+// number = 10;
+// console.log(number);
+// const obj = {
+//     a:50
+// };
+
+
+// obj.a =10;
+// console.log(obj);
+// {
+//     var result = 50;
+// }
+// console.log(result);
+
+
+// const a = 5000;
+// const b = 3000;
+// console.log( "car width : "+ a + "   car length : " + b);
+// const obj = {name:'John', age:25, isMarried:false};
+// console.log(obj.isMarried);
+// console.log(obj['isMarried']);
+// let arr = ['zaebalo', 'uzhe', 'vse', 'ya' , 'ne mogu', 'nahooi'];
+// console.log();
+
+
+// const arr = [1, 2, 3];
+// console.log(arr[2]);
+// const obj = {0:500, "Alice":800};
+// obj.sosi = 228;
+// console.log(obj);
+// console.log(obj['sosi']);
+// const answers = [];
+// answers[0] = prompt("what is your name?", "");
+// answers[1] = prompt("what is your surname?", "");
+// answers[2] = prompt("what is your age?", "");
+
+// document.write(typeof(answers));
+
+
+
+//  interpolyacia
+// const user = prompt("what is your name", "");
+// document.write(`${user}, penis is your real name`);
+
+// operators
+// let incr = 10,
+//     decr = 10;
+// // ++incr;
+// // --decr;
+
+
+// console.log("incr: "+ incr++ +" decr: "+ decr--);
+// console.log("incr: " +  ++incr +" decr: "+ --decr);
+let num1, num2;
+num1 = 14;
+num2 = 9 ;
+console.log(num1.toString(2));
